ad2e5bc59ec09c4f51319d9cd9f290afb7105c8b
Ryoooun <ryosuke.yamashit4@gmail.com>
2023-03-22 03:12:05 +0900

    commit

--- app/common/icon/homeMenuIcons.js
+++ app/common/icon/homeMenuIcons.js
@@ -1,3 +1,4 @@
 'use client'
 export {FiBook} from '@react-icons/all-files/fi/FiBook'
 export {FiSearch} from '@react-icons/all-files/fi/FiSearch'
+export {ImLoop2} from '@react-icons/all-files/im/ImLoop2'
