3c0e76b53a4df8c3709fbfb106dc57d32ceaaf70
RyosukeYamashita <ryosuke.yamashit4@gmail.com>
2023-03-27 17:22:01 +0900

    local env 実装

--- app/auth/firebase.js
+++ app/auth/firebase.js
@@ -6,12 +6,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // webアプリのFirebase設定
 const firebaseConfig = {
-  apiKey: "AIzaSyCmcav6JtvJYFLYfbx_Ki4xa9qKiFjvDv4",
-  authDomain: "sublim214.firebaseapp.com",
-  projectId: "sublim214",
-  storageBucket: "sublim214.appspot.com",
-  messagingSenderId: "738134900457",
-  appId: "1:738134900457:web:c266a91f7e5a6435297a78",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Firebaseの初期化
