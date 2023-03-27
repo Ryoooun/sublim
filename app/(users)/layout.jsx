e6f702c76efb8e4c59f92a5500b39416a2ee381d
RyosukeYamashita <ryosuke.yamashit4@gmail.com>
2023-03-27 16:43:57 +0900

    not fount page 実装

--- app/(users)/layout.jsx
+++ app/(users)/layout.jsx
@@ -1,10 +1,18 @@
+"use client";
 import Menu from "./user/components/template/Menu";
+import { useIsAuth } from "../store/auth";
+import { notFound } from "next/navigation";
 
 export default function UserLayout({ children }) {
-  return (
-    <>
-      <Menu />
-      {children}
-    </>
-  );
+  const isAuth = useIsAuth((state) => state.isAuth);
+  if (isAuth) {
+    return (
+      <>
+        <Menu />
+        {children}
+      </>
+    );
+  } else {
+    notFound();
+  }
 }
--- app/hooks/useUser.js
+++ app/hooks/useUser.js
@@ -1,23 +1,30 @@
 import { getAuth } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
 export const useUser = () => {
-  const auth = getAuth();
-  const user = auth.currentUser;
+  try {
+    const auth = getAuth();
+    const user = auth.currentUser;
 
-  if (user !== null) {
-    const email = user.email;
-    const accessToken = user.accessToken;
-    const userName = user.displayName;
-    const photoURL = user.photoURL;
-    const userInfo = {
-      userName,
-      email,
-      accessToken,
-      photoURL,
-    };
+    if (user !== null) {
+      const email = user.email;
+      const accessToken = user.accessToken;
+      const userName = user.displayName;
+      const photoURL = user.photoURL;
+      const userInfo = {
+        userName,
+        email,
+        accessToken,
+        photoURL,
+      };
 
-    return userInfo;
-  } else {
-    return { user };
+      return userInfo;
+    } else {
+      return { user };
+    }
+  } catch (err) {
+    console.log(err);
+    const router = useRouter();
+    router.push("/");
   }
 };
