5ea07803027b9c6ad56e9c48e53bac40f78bab29
RyosukeYamashita <ryosuke.yamashit4@gmail.com>
2023-04-03 17:45:40 +0900

    post

--- app/(users)/user/[id]/trend/components/organisms/QiitaPostList.jsx
+++ app/(users)/user/[id]/trend/components/organisms/QiitaPostList.jsx
@@ -11,11 +11,15 @@ import {
   Divider,
   useMediaQuery,
 } from "@/app/common/chakraui/ChakraUI";
-import React, { useMemo } from "react";
-import "../../../../components/organisms/scroll.css";
+import React, { useMemo, useState } from "react";
 
+import { motion } from "framer-motion";
+import "../../../../components/organisms/scroll.css";
+import "./styles.css";
 export default React.memo(function QiitaPostList({ qiitaItems }) {
   const [isLargerThen50em] = useMediaQuery("(min-width: 50em)");
+  const [isOpen, setIsOpen] = useState(false);
+
   const posts = [...qiitaItems];
 
   return (
@@ -30,19 +34,35 @@ export default React.memo(function QiitaPostList({ qiitaItems }) {
         {posts.map((post) => {
           return (
             <Box
+              as={motion.div}
+              layout
+              data-isOpen={isOpen}
+              initial={{ borderRadius: "xl" }}
+              onClick={() => setIsOpen(!isOpen)}
+              className="Card"
               key={post.id}
-              boxSizing="border-box"
-              mb="2"
-              minW={isLargerThen50em ? "25%" : "60%"}
+              mb="4"
+              minW={isLargerThen50em ? "28%" : "60%"}
               borderRadius="xl"
-              bg="blackAlpha.50"
               scrollSnapAlign="center"
               scrollSnapStop="always"
-              boxShadow="0 20px 25px -5px rgba(0 0 0 / .10), 0 10px 10px -5px rgba(0 0 0 / .0.4)"
-              _hover={{ bg: "blackAlpha.200" }}
-              p="4">
+              // boxShadow="0 20px 25px -5px rgba(0 0 0 / .10), 0 10px 10px -5px rgba(0 0 0 / .0.4)"
+              sx={{
+                boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+              }}
+              p="4"
+              _hover={{
+                opacity: "0.7",
+                transitionDuration: "400ms",
+                transitionTimingFunction: "ease-in-out",
+              }}>
               <Box whiteSpace="normal">
-                <Avatar name={post.user.id} src={post.user.profile_image_url} />
+                <Heading>Keyword</Heading>
+                <Avatar
+                  name={post.user.id}
+                  src={post.user.profile_image_url}
+                  size="md"
+                />
                 <Text
                   fontSize="sm"
                   fontFamily="mono">{`@${post.user.id}`}</Text>
@@ -51,22 +71,22 @@ export default React.memo(function QiitaPostList({ qiitaItems }) {
                     {post.title}
                   </Heading>
                 </a>
-                <Divider w="full" />
-              </Box>
-              <Box maxH="4rem">
-                <Flex direction="row" flexWrap="wrap" gap="1">
-                  {post.tags.map((tag, i) => (
-                    <Box
-                      fontSize="xs"
-                      px="0.2rem"
-                      bg="blackAlpha.400"
-                      color="white"
-                      borderRadius="md"
-                      key={i}>
-                      {tag}
-                    </Box>
-                  ))}
-                </Flex>
+                <Divider w="full" mt="3" />
+                <Box mt="3" maxH="4rem">
+                  <Flex direction="row" flexWrap="wrap" gap="1">
+                    {post.tags.map((tag, i) => (
+                      <Box
+                        fontSize="xs"
+                        px="0.2rem"
+                        bg="blackAlpha.400"
+                        color="white"
+                        borderRadius="md"
+                        key={i}>
+                        {tag}
+                      </Box>
+                    ))}
+                  </Flex>
+                </Box>
               </Box>
             </Box>
           );
