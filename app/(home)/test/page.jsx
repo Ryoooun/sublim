d8c544e86906707395bd9f3a05faf93760c522ec
Ryoooun <ryosuke.yamashit4@gmail.com>
2023-04-05 02:29:15 +0900

    emotion framer-motion test

--- app/(home)/test/page.jsx
+++ app/(home)/test/page.jsx
@@ -1,47 +1,63 @@
-import { load } from "cheerio";
-import axios from "axios";
+"use client";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-async function getData() {
-  const url = "https://blog.kapiecii.com/posts/";
-  console.time("c");
-  const res = await fetch(url).catch((err) => console.error(err));
-  const html = await res.text();
-  const $ = await load(html);
-  const data = [];
-  $(".post-item", html).each((item, element) => {
-    const title = $(element)
-      .find("a")
-      .text()
-      .replace(/(\r\n|\n|\r|)/gm, "")
-      .trim();
-    data.push(title);
-  });
-  console.timeEnd("c");
-  return data;
+import { Button, Box, Center } from "@/app/common/chakraui/ChakraUI";
+import { motion, AnimatePresence, useAnimate } from "framer-motion";
+import { useState, useEffect } from "react";
 
-  // const data = await axios.get(url).then((res) => res.data);
-  // const $ = await load(data);
-  // const result = [];
-  // $(".post-item", data).each((item, element) => {
-  //   const title = $(element)
-  //     .find("a")
-  //     .text()
-  //     .replace(/(\r\n|\n|\r|)/gm, "")
-  //     .trim();
-  //   result.push(title);
-  // });
+const spring = {
+  type: "spring",
+  stiffness: "800",
+  damping: 30,
+};
 
-  // console.timeEnd("c");
-  // return result;
-}
+const handle = css`
+  width: 80px;
+  height: 80px;
+  background-color: white;
+  border-radius: 40px;
+`;
 
-export default async function page() {
-  const data = await getData();
+const switch_ = css`
+  width: 160px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 0.4);
+  display: flex;
+  justify-content: flex-start;
+  border-radius: 50px;
+  padding: 10px;
+  cursor: pointer;
+  &[data-ison="true"] {
+    justify-content: flex-end;
+  }
+`;
+
+export default function page(params) {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <ul>
-      {data.map((d, i) => (
-        <li key={i}>{d}</li>
-      ))}
-    </ul>
+    <Center>
+      <Box
+        w="90vw"
+        h="90vh"
+        bg="green.100"
+        mt="10"
+        display="grid"
+        placeItems="center">
+        <Button
+          onClick={() => setToggle((prev) => !prev)}
+          pos="absolute"
+          top="20">
+          Toggle
+        </Button>
+        <div
+          css={switch_}
+          data-ison={toggle}
+          onClick={() => setToggle((prev) => !prev)}>
+          <motion.div css={handle} layout transition={spring} />
+        </div>
+      </Box>
+    </Center>
   );
 }
