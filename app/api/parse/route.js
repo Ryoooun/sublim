6beb6e6bf8721ca5e3667ad87f0f4a8675bd6907
Ryoooun <ryosuke.yamashit4@gmail.com>
2023-04-06 00:28:42 +0900

    set

--- app/api/parse/route.js
+++ app/api/parse/route.js
@@ -23,7 +23,7 @@ export async function GET(request) {
     return new Promise((resolve, reject) => {
       kuromoji
         .builder({
-          dicPath: "/node_modules/kuromoji/dict",
+          dicPath: "/public/dict",
         })
         .build((err, tokenizer) => {
           if (err) {
