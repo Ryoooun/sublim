07b3b08e45fefe4d44262a31659e62a3282838b4
RyosukeYamashita <ryosuke.yamashit4@gmail.com>
2023-04-06 16:50:34 +0900

    map page

---
+++ app/(users)/user/[id]/map/components/template/PageWrapper.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+
+import {
+  Heading,
+  Box,
+  VStack,
+  useMediaQuery,
+} from "@/app/common/chakraui/ChakraUI";
+export default React.memo(function PageWrapper({ children }) {
+  const [isLargerThen50em] = useMediaQuery("(min-width: 50em)");
+  return (
+    <Box
+      overflow="hidden"
+      w={isLargerThen50em ? "93vw" : "100vw"}
+      bg="white"
+      // px={isLargerThen50em ? "4rem" : "5"}
+      py={isLargerThen50em ? "0" : "10"}
+      pb="0">
+      {children}
+    </Box>
+  );
+});
--- app/(users)/user/[id]/map/page.jsx
+++ app/(users)/user/[id]/map/page.jsx
@@ -1,89 +1,45 @@
 "use client";
 
-import { useLogout } from "@/app/hooks/useLogout";
+import PageWrapper from "./components/template/PageWrapper";
+import { Heading, Container, Box } from "@/app/common/chakraui/ChakraUI";
 
-import {
-  Button,
-  Card,
-  CardBody,
-  CardHeader,
-  Heading,
-  Divider,
-  useMediaQuery,
-} from "../../../../common/chakraui/ChakraUI";
-import { useUser } from "@/app/store/user";
+import { motion, LayoutGroup } from "framer-motion";
+import { useState } from "react";
 
 export default function page(params) {
-  const user = useUser((state) => state.user);
-  const [isLargerThen50em] = useMediaQuery("(min-width: 50em)");
-
-  const LoginUser = () => {
-    if (user) {
-      return (
-        <>
-          <p>{`${user.displayName}`}'s Map</p>
-        </>
-      );
-    } else {
-      return <p>こんにちは!ゲストさん</p>;
-    }
-  };
-
-  const TestFlowContent = () => {
+  const ToggleContent = ({ header, content }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    console.log();
     return (
-      <>
-        {Array(10)
-          .fill(1)
-          .map((index) => {
-            return (
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint
-                error provident molestias molestiae fugit quae quam inventore
-                omnis aspernatur repellendus dolorem soluta, quibusdam ea
-                obcaecati veritatis corporis expedita a numquam?
-              </p>
-            );
-          })}
-      </>
+      <motion.div layout onClick={() => setIsOpen(!isOpen)}>
+        <Heading as={motion.h1} layout>
+          {header}
+        </Heading>
+        {isOpen ? (
+          <Box fontSize="lg" bg="blackAlpha.200">
+            {content}
+          </Box>
+        ) : null}
+      </motion.div>
     );
   };
-
-  const TestBoxContent = () => {
-    return (
-      <>
-        {Array(10)
-          .fill(1)
-          .map((_, index) => {
-            return (
-              <Card backdropFilter="auto" backdropBlur="2px">
-                <CardHeader>
-                  <h5>title{index}</h5>
-                </CardHeader>
-                <CardBody>
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Quibusdam, eum cum illum velit laboriosam deserunt incidunt
-                    maiores sapiente accusamus similique numquam, corrupti
-                    accusantium voluptates voluptatibus sint asperiores, cumque
-                    officiis dignissimos.
-                  </p>
-                </CardBody>
-              </Card>
-            );
-          })}
-      </>
-    );
-  };
-
   return (
-    <>
-      <LoginUser />
-      <Card boxShadow="xs">
-        <CardHeader>
-          <Heading></Heading>
-          <Divider w="full" />
-        </CardHeader>
-      </Card>
-    </>
+    <PageWrapper>
+      <Container display="flex" flexDirection="column" alignItems="center">
+        <Heading>Map page</Heading>
+        <Box bg="brand.200" w="45rem" h="45rem">
+          <LayoutGroup>
+            <ToggleContent
+              header="Title1"
+              content="content1content1content1content1content1content1content1"
+            />
+            <ToggleContent
+              header="Title2"
+              content="content2content2content2content2content2content2content2"
+            />
+          </LayoutGroup>
+        </Box>
+      </Container>
+    </PageWrapper>
   );
 }
