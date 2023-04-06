9f50a65885af3fc5222dc1b654b3c8d86cad58fe
RyosukeYamashita <ryosuke.yamashit4@gmail.com>
2023-04-06 10:40:37 +0900

    d

--- app/api/parse/route.js
+++ app/api/parse/route.js
@@ -5,59 +5,72 @@ import termextract from "@/app/lib/js/termextract-kuromojijs";
 import { cwd, env } from "node:process";
 import module from "node:module";
 import path from "node:path";
+import * as fs from "fs";
 
 export async function GET(request) {
-  const { searchParams } = new URL(request.url);
-  const url = searchParams.get("url");
+  // const { searchParams } = new URL(request.url);
+  // const url = searchParams.get("url");
 
-  const getData = async (url) => {
-    const res = await fetch(url);
-    const html = await res.text();
-    const $ = await load(html);
-    return await $("p").text();
-  };
+  // const getData = async (url) => {
+  //   const res = await fetch(url);
+  //   const html = await res.text();
+  //   const $ = await load(html);
+  //   return await $("p").text();
+  // };
 
+  // const strings = await getData(url);
+  // const kuro = () => {
+  //   return new Promise((resolve, reject) => {
+  //     kuromoji
+  //       .builder({
+  //         dicPath: "/var/task/.next/server/app/lib/dict",
+  //       })
+  //       .build((err, tokenizer) => {
+  //         if (err) {
+  //           reject(err);
+  //         }
+  //         resolve(tokenizer);
+  //       });
+  //   });
+  // };
+
+  // const json = await kuro().then((tokenizer) => {
+  //   let tokenized_word = tokenizer.tokenize(strings);
+  //   let cmp_noun_list = termextract.cmp_noun_list(tokenized_word);
+  //   let frequency = termextract.list2key_value(cmp_noun_list);
+  //   let score_lr = termextract.score_lr(
+  //     frequency,
+  //     termextract.IGNORE_WORDS,
+  //     1,
+  //     1
+  //   );
+  //   let term_importance = termextract.term_importance(frequency, score_lr);
+  //   let score_lt_list = termextract.sort_by_importance(term_importance);
+  //   const array = [];
+  //   for (const data of score_lt_list) {
+  //     const word = termextract.modify_agglutinative_lang(data.cmp_noun);
+  //     array.push({ [word]: data.importance });
+  //   }
+  //   return array;
+  // });
   const require = module.createRequire(import.meta.url);
-  const strings = await getData(url);
-  const kuro = () => {
-    return new Promise((resolve, reject) => {
-      kuromoji
-        .builder({
-          dicPath: "/var/task/.next/server/app/lib/dict",
-        })
-        .build((err, tokenizer) => {
-          if (err) {
-            reject(err);
-          }
-          resolve(tokenizer);
-        });
-    });
-  };
-
-  const json = await kuro().then((tokenizer) => {
-    let tokenized_word = tokenizer.tokenize(strings);
-    let cmp_noun_list = termextract.cmp_noun_list(tokenized_word);
-    let frequency = termextract.list2key_value(cmp_noun_list);
-    let score_lr = termextract.score_lr(
-      frequency,
-      termextract.IGNORE_WORDS,
-      1,
-      1
-    );
-    let term_importance = termextract.term_importance(frequency, score_lr);
-    let score_lt_list = termextract.sort_by_importance(term_importance);
-    const array = [];
-    for (const data of score_lt_list) {
-      const word = termextract.modify_agglutinative_lang(data.cmp_noun);
-      array.push({ [word]: data.importance });
-    }
-    return array;
-  });
-
+  const e = path.resolve(require.resolve("kuromoji"), "../../dict");
   const d = __dirname;
   const f = __filename;
   const a = process.env.PATH;
-  return NextResponse.json({ data: "hello", d, f, a, url });
+  const c = process.cwd();
+  // const Afiles = fs.readFileSync(a);
+  // const Cfiles = fs.readFileSync(c);
+
+  return NextResponse.json({
+    dirName: d,
+    fileName: f,
+    Path: a,
+    cwd: c,
+    e,
+    // Afiles,
+    // Cfiles,
+  });
   // const path = process.cwd();
   // return NextResponse.json({ data: path });
 }
